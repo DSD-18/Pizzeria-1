dc3dac5b476afdc59b89cc3f819c4f1d056d717d
DSD-18 <72644829+DSD-18@users.noreply.github.com>
2020-11-30 18:50:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var menu_list_array = ["Chicken Tandoori pizza", "Panneer Ticka pizza"," Deluxe Veggie pizza"]
+function add_item(){
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata = "<section class = 'cards'> "
+    for(var i = 0;i<menu_list_array.length;i++){
+            htmldata-htmldata+ '<div class="card">' + '<img scr="images/pizzaIng.png"/>' + menu_list_array[i] + '</div'
+        }
+        htmldata = htmldata + "</section>"
+        document.getElementById("display_addmenu").innerHTML = htmldata;
+}
